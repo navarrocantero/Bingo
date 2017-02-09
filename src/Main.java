4c85db3b97bc5b4f0b1c8b6d871097d311991ca5
navarrocantero <navarrocantero@gmail.com>
2017-02-09 23:33:44 +0100

    update 3, funcionando

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
-
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
-
 public class Main {
-
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
-
         //CONSTANTS
         final int MIN   = 1;            // Se usa para tener contabilizado el mínimo número de bolas.
         final int MAX   = 90;           // Se usa para tener contabilizado el máximo número  de bolas.
@@ -21,97 +16,123 @@ public class Main {
         //VARIABLES
         // Integers
         int numbr;                      // Variable que indica el número de cartones a jugar.
-        int count = 0;                  // Variable que controla el número de bingos.
+        int count = 0;
+        int ballsUse = 0;
+        // Variable que controla el número de bingos.
         //ARRAYS
-        int plays[][];                          // Array que contendrá la cantidad de cartones con los que se jugará con la cantidad de casillas que se rellenarán de números en cada cartón.
-        int lineWin[][];                        //
-        int bingWin[][];                        //
-        int ballsSecuence [] = new int[MAX];    // en este array se recogen el orden de las 90 bolas que saldrán.
 
-
-        System.out.println("\n Welcome to the Classic Bingo \n");
-        System.out.printf(" Insert number of games : ");
-
-        numbr = input.nextInt();            // Insert number of game plays. Número de cartones con los que se va a jugar.
-
-        plays = new int[numbr][NUMS];       // ( CREO QUE ESTO SE PODRÍA QUITAR: and select for two arrays ). Cada cartón con sus 15 espacios.
+        int plays[][];
+        int lineWin[][];
+        int bingWin[][];
+        int ballsSecuence[] = new int[MAX];
+        int ballsOut[] = new int[MAX];// en este array se recogen el orden de las 90 bolas que saldrán.
+        System.out.println("\n Bienvenido al Bingo clásico \n");
+        do {
+            System.out.printf(" Insert number of games : ");
+            numbr = input.nextInt();            // Insert number of game plays. Número de cartones
+            if (numbr<0) {
+                System.out.println(" Porfavor vuelva a introducir un valor que no sea negativo");
+                System.out.println(" ");
+            }
+        }while (numbr<0);
+        plays = new int[numbr][NUMS];       // and select for two arrays. Cada cartón con sus 15 espacios.
         lineWin = new int[numbr][POS];      // in first dimension the prev Int
         bingWin = new int[numbr][ROWS];
-
-        // Llamada a la función que rellena cada uno de los cartones con números aleatorios de menor a mayor sin que se repitan.
+        // Se rellena cada uno de los cartones con números aleatorios de menor a mayor sin que se repitan
         for (int i = 0; i < plays.length; i++) {
-            fillSortedArray (plays[i], NUMS, MIN, MAX);
+            fillSortedArray(plays[i], NUMS, MIN, MAX);
         }
-
-        // Función para imprimir el cartón o los cartones con los que se juega.
-        printArray (plays, POS, ROWS);
-
-        // Función que simula el sacar las bolas.
-        balls (ballsSecuence, MAX);
+        //  Función para imprimir el cartón o los cartones con los que se juega
+        printArray(plays, POS);
+        //  Función que simula el sacar las bolas.
+        balls(ballsSecuence, MAX);
         //  En este bucle se muestran las bolas que van saliendo, que ya están puestas en el array ballsSecuence
         for (int i = 0; i < ballsSecuence.length; i++) {    // este bucle hace que salgan las 90 bolas, poner || ballsSecuence == false
-            if (count != numbr) { // a este punto count sólo vale 0, ver si se actualiza si valor más abajo
+            if (count != 1) {
+                ballsOut[i] = ballsSecuence[i];
+                ballsUse=i;
                 System.out.println("La bola número " + ((i + 1) + " es : " + ballsSecuence[i]));
                 for (int j = 0; j < numbr; j++) {               // Bucle para operar en cada uno de los cartones
                     for (int m = 0; m < NUMS; m++) {            //  Bucle para operar con cada una de las posiciones del cartón en el que nos encontramos
                         if (ballsSecuence[i] == plays[j][m]) {  // Comprobar bola con todas las posiciones del cartón
-                            if (m >= THIRDLINE) {               //Las 5 últimas posiciones
+
+
+                            if (m >= THIRDLINE) {               //Las 5 últimas posiciones m hace referencia a la posicion del for anterior.
                                 lineWin[j][2]++;                // si la bola pertenece a la tercera linea, el numero de carton J la linea 2 ++
-                                System.out.println("Encontrado en el cartón " + (j + 1) + " en la linea 3");
+                                System.out.println("Encontrado en el cartón " + (j + 1) + " en la linea 3 "+ "\n" );
                                 if (lineWin[j][2] == 5) {       // cuando lo de arriba valga 5 tenemos linea
                                     bingWin[j][0]++;
                                     if (bingWin[j][0] != 3) {
-                                        System.out.println("Linea 3 en el cartón " + (j + 1));
+                                        System.out.println("Linea 3 en el cartón " + (j + 1) + "\n");
                                     }
                                 }
                             } else if (m >= SECONDLINE && m < THIRDLINE) {       //Las posiciones entre 5 y 10 del cartón
                                 lineWin[j][1]++;
-                                System.out.println("Encontrado en el cartón " + (j + 1) + "  en la linea 2");
+                                System.out.println("Encontrado en el cartón " + (j + 1) + "  en la linea 2"+ "\n" );
                                 if (lineWin[j][1] == 5) {
                                     bingWin[j][0]++;
                                     if (bingWin[j][0] != 3) {
-                                        System.out.println("Linea 2 en el cartón " + (j + 1));
+                                        System.out.println("Linea 2 en el cartón " + (j + 1) + "\n");
                                     }
                                 }
-                            } else if (m >= FIRSTLINE && m < SECONDLINE) {        //Las primeras 5 posiciones del cartón
+                            } else{      //Las primeras 5 posiciones del cartón
                                 lineWin[j][0]++;
-                                System.out.println("Encontrado en el cartón " + (j + 1) + "  en la linea 1");
+                                System.out.println("Encontrado en el cartón " + (j + 1) + "  en la linea 1"+ "\n" );
                                 if (lineWin[j][0] == 5) {
                                     bingWin[j][0]++;
                                     if (bingWin[j][0] != 3) {
-                                        System.out.println("Linea 1 en el cartón " + (j + 1));
+                                        System.out.println("Linea 1 en el cartón " + (j + 1) + "\n");
                                     }
-                                }
-                            }
-                            for (int k = 0; k < ROWS; k++) {
-                                if (bingWin[j][k] == ROWS) {
-                                    System.out.println("Bingo en el cartón " + (j + 1));
-                                    count++;
                                 }
                             }
                         }   // Fin comprobar bola con todas las posiciones del cartón
                     }
                 }
+                for (int j = 0; j < numbr; j++) {           //Está aquí para esperar que todos los cartones miren si tienen el número y despues comprueba si tienen todos bingo
+                    for (int k = 0; k < ROWS; k++) {
+                        if (bingWin[j][k] == ROWS) {
+                            if (count!=1) {
+                                count++;
+                                System.out.println("Bingo"+"\n");
+                                System.out.println("");
+                            }
+                            System.out.println("El cartón ganador es el " + (j + 1) + "\n");
+
+                            for (int l = 0; l < plays[j].length; l++) {
+                                System.out.printf("%3d",plays[j][l]);
+                                if ((l+1) % POS == 0){
+                                    System.out.println();
+                                }
+                            }
+                            System.out.println(" ");
+                            System.out.println(" ");
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(" ");
+        System.out.println("Y las bolas que han salido son:\n");      //Esta aqui para que no haya problema con las demás bolas que salen y no son vistas
+        for (int l = 0; l <= ballsUse; l++) {
+            // if (ballsSecuence[l] != 0)
+            //{
+            System.out.printf("%3d",ballsSecuence[l] );
+            if ((l+1) % (POS+POS) == 0){
+                System.out.println();
             }
         }
     }
 
-    /**
-     * Función que rellena la cantidad de cartones con los que se va a jugar, con números ordendos aleatorios de menor a mayor sin repeticiones por cada vez que se llame a esta función.
-     * @param plays
-     * @param numsCopy
-     * @param minCopy
-     * @param maxCopy
-     */
-    static void fillSortedArray(int[] plays, int numsCopy, int minCopy, int maxCopy) {  // En esta funcón se rellena un cartón, si hay más cartones se rellenar con el incremento de i el el bucle que llama a esta función
+    // crea un cartón con números ordendos aleatorios de menor a mayor sin repeticiones por cada vez que se llame a esta función
+    static void fillSortedArray(int[] plays, int numsCopy, int minCopy, int maxCopy) {
         int arrayCopy[] = new int[numsCopy];
         boolean bol = false;
         do {
-            for (int j = 0; j < arrayCopy.length; j++) {    //generate numbers
+            for (int j = 0; j < arrayCopy.length; j++) {                //generate numbers
                 arrayCopy[j] = (int) (Math.random() * (maxCopy) + minCopy);
             }
-            Arrays.sort(arrayCopy);                         //order numbers
-            for (int k = 1; k < arrayCopy.length; k++) {    //Check that any number content in array is repeated
+            Arrays.sort(arrayCopy);                                     //order numbers
+            for (int k = 1; k < arrayCopy.length; k++) {                    //Check that any number content in array is repeated
                 if (arrayCopy[k] == arrayCopy[k - 1]) {
                     bol = false;
                     break;
@@ -120,17 +141,16 @@ public class Main {
                 }
             }
         } while (!bol);
-        for (int c = 0; c < arrayCopy.length; c++) {         // When all numbers content in arrayCopy are different,
-            plays[c] = arrayCopy[c];                         // we charge the arraycopy  to our principal array
+        for (int c = 0; c < arrayCopy.length; c++) {             // When all numbers content in arrayCopy are different,
+            plays[c] = arrayCopy[c];                            // we charge the arraycopy  to our principal array
         }
     }
     /**
      * Función para imprimir el cartón o los cartones con los que se juega
      * @param array es el array plays, que vienen a ser los cartones
      * @param colsCopy: copia de POS
-     * @param rowsCopy  Copia de las filas
      */
-    static void printArray(int array[][], int colsCopy, int rowsCopy) {
+    static void printArray(int array[][], int colsCopy) {
         for (int i = 0; i < array.length; i++) {
             System.out.println("\n Game number " + (i + 1));
             System.out.println();
